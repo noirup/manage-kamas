3f5a13c5dbe200aa28ba2f527bfce8b966d2acaf
Raphaël SEEGER <raphael.seeger@student.vinci.be>
2021-02-03 02:27:20 +0100

    changed proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
     app.use(
         '/api',
         createProxyMiddleware(
-            {target : 'https://manage-kamas-api.herokuapp.com/',
+            {target : 'https://manage-kamas-api.herokuapp.com',
             pathRewrite : {'/api/': '/'}}
         )
     );
